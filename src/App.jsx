207db30b73b858bbb690f66ff567848b46a397db
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-10-11 21:32:29 +0100

    initialize React app with Vite setup and basic styling

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./assets/spotify.png"
+
+function App() {
+  return (
+    <div className="parent">
+      <h1>Hello world</h1>
+      <img src={logo} alt="logo" />
+    </div>
+  )
+}
+
+export default App
